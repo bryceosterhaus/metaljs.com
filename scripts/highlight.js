85e965f5264b5bb35888040677648621b4fcbf21
Zeno Rocha <hi@zenorocha.com>
2015-07-20 15:05:45 -0700

    Update 2015-07-20T22:05:42.714Z

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,4 +1,14 @@
-var myCodeMirror = CodeMirror(document.body, {
-	value: "function myScript(){return 100;}\n",
-	mode:  "javascript"
-});
+var code = document.querySelectorAll('.code');
+
+for (var i = 0; i < code.length; i++) {
+	CodeMirror.fromTextArea(code[i], {
+		mode: code[i].dataset.mode ? 'text/' + code[i].dataset.mode : '',
+		lineNumbers: true,
+		matchBrackets: true,
+		indentUnit: 2,
+		indentWithTabs: false,
+		theme: 'dracula',
+		viewportMargin: Infinity,
+		readOnly: true
+	});
+}
