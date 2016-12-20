b34d44a7d5d42f69c15996257163a75bc5437626
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-12-20 15:00:24 -0800

    Update 2016-12-20T23:00:19.981Z

--- downloads/boilerplate-jsx/2. Modal/test/Modal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Modal from '../src/Modal';
-
-describe('Modal', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
-	});
-});
--- downloads/boilerplate/1. Hello World/gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var metal = require('gulp-metal');
-
-metal.registerTasks({
-	bundleCssFileName: 'modal.css',
-	bundleFileName: 'modal.js',
-	moduleName: 'metal-modal'
-});
